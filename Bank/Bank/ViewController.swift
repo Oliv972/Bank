b29a680ddf1b17cedea9358658aec17e360dfa7b
o.adin@arkadin.com <o.adin@arkadin.com>
2019-09-11 09:57:25 +0200

    Initial commit

---
+++ Bank/Bank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bank
+//
+//  Created by Olivier Adin on 11/09/2019.
+//  Copyright © 2019 Olivier Adin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
