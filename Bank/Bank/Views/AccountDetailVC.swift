49b2cc2839031a54ae659cd726bb6bebe6b72396
o.adin@arkadin.com <o.adin@arkadin.com>
2019-09-11 10:51:49 +0200

    Better image

---
+++ Bank/Bank/Views/AccountDetailVC.swift
@@ -0,0 +1,9 @@
+//
+//  AccountDetailVC.swift
+//  Bank
+//
+//  Created by Olivier Adin on 11/09/2019.
+//  Copyright © 2019 Olivier Adin. All rights reserved.
+//
+
+import Foundation
---
+++ Bank/Bank/Views/AccountsVC.swift
@@ -0,0 +1,9 @@
+//
+//  AccountsVC.swift
+//  Bank
+//
+//  Created by Olivier Adin on 11/09/2019.
+//  Copyright © 2019 Olivier Adin. All rights reserved.
+//
+
+import Foundation
